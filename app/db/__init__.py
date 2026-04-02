689443a0ca71832ca6769c22aebc0ecd22c3edec
jrip <mail@jrlab.ru>
2026-04-03 02:48:59 +0300

    fix

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,5 +1,4 @@
 from app.db.config import Base
 from app.db.database import SessionLocal, engine, get_db, get_engine
-from app.db.seed import run_seed
 
-__all__ = ("Base", "SessionLocal", "engine", "get_db", "get_engine", "run_seed")
+__all__ = ("Base", "SessionLocal", "engine", "get_db", "get_engine")
